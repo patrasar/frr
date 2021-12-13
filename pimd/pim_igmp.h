079b587d6fa32a82f1826f1e5f1fb62f3f282143
sarita patra <saritap@vmware.com>
2021-12-13 06:03:31 -0800

    pimd: Modifying in_addr to PIM_ADDR in struct igmp_group for IPv6.
    Changed struct in_addr group_addr to struct PIM_ADDR group_addr
    in struct igmp_group which is to be used in
    both IPv4 and IPv6(Both MLD and IGMP).
    
    Reviewed-by: Mobashshera Rasool <mrasool@vmware.com>
    Signed-off-by: sarita patra <saritap@vmware.com>

--- pimd/pim_igmp.h
+++ pimd/pim_igmp.h
@@ -26,6 +26,7 @@
 #include "vty.h"
 #include "linklist.h"
 #include "pim_igmp_stats.h"
+#include "pim_str.h"
 
 /*
   The following sizes are likely to support
@@ -175,8 +176,7 @@ struct igmp_group {
 
 	/* compatibility mode - igmp v1, v2 or v3 */
 	int igmp_version;
-
-	struct in_addr group_addr;
+	struct PIM_ADDR group_addr;
 	int group_filtermode_isexcl;    /* 0=INCLUDE, 1=EXCLUDE */
 	struct list *group_source_list; /* list of struct igmp_source */
 	time_t group_creation;
