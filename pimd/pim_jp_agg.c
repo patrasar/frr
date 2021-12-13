2da6c768b5bf33e21c37882e9990a17ebc985632
sarita patra <saritap@vmware.com>
2021-12-13 06:01:36 -0800

    pimd: Modifying in_addr to PIM_ADDR in struct pim_iface_upstream_switch… … for IPv6.
    Changed struct in_addr address to struct PIM_ADDR in struct
    pim_iface_upstream_switch which is to be used in both IPv4
    and IPv6(Both MLD and IGMP).
    
    Reviewed-by: Mobashshera Rasool <mrasool@vmware.com>
    Signed-off-by: sarita patra <saritap@vmware.com>

--- pimd/pim_jp_agg.c
+++ pimd/pim_jp_agg.c
@@ -23,6 +23,7 @@
 #include "log.h"
 #include "vrf.h"
 #include "if.h"
+#include "prefix.h"
 
 #include "pimd.h"
 #include "pim_msg.h"
@@ -133,14 +134,14 @@ pim_jp_agg_get_interface_upstream_switch_list(struct pim_rpf *rpf)
 
 	for (ALL_LIST_ELEMENTS(pim_ifp->upstream_switch_list, node, nnode,
 			       pius)) {
-		if (pius->address.s_addr == rpf->rpf_addr.u.prefix4.s_addr)
-			break;
+		if (pim_addr_is_same(pius->address, PIM_UN_PFX(rpf->rpf_addr)))
+		break;
 	}
 
 	if (!pius) {
 		pius = XCALLOC(MTYPE_PIM_JP_AGG_GROUP,
 			       sizeof(struct pim_iface_upstream_switch));
-		pius->address.s_addr = rpf->rpf_addr.u.prefix4.s_addr;
+		pim_addr_copy(&pius->address, &PIM_UN_PFX(rpf->rpf_addr));
 		pius->us = list_new();
 		listnode_add_sort(pim_ifp->upstream_switch_list, pius);
 	}
